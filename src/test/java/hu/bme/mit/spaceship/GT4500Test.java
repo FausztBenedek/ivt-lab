6b2f9b268705d019ef7e2cc00fe6f16d566ff333
Fauszt Benedek <fausztb@gmail.com>
2020-05-01 12:34:55 +0200

    UnitTesting: Isolation of GT4500 + correcting its unit test

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -3,38 +3,55 @@ package hu.bme.mit.spaceship;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Any;
 
 import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore secondaryTorpedoStore;
+  private TorpedoStore primaryTorpedoStore;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+     primaryTorpedoStore = mock(TorpedoStore.class);
+     secondaryTorpedoStore = mock(TorpedoStore.class);
+
+    this.ship = new GT4500(primaryTorpedoStore, secondaryTorpedoStore);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(primaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(primaryTorpedoStore.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+
+    verify(primaryTorpedoStore, times(1)).fire(1);
+    verify(secondaryTorpedoStore, never()).fire(anyInt());
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(primaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(secondaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(primaryTorpedoStore.fire(1)).thenReturn(true);
+    when(secondaryTorpedoStore.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(primaryTorpedoStore, times(1)).fire(1);
+    verify(secondaryTorpedoStore, times(1)).fire(1);
   }
 
 }
